1f360128513854d64b82ab0496c075323d5cc9ae
Anant yadav <143705250+RAOsahab001@users.noreply.github.com>
2024-05-04 22:23:18 +0530

    Create ice_creram.py

---
+++ Problem-4/ice_creram.py
@@ -0,0 +1,7 @@
+def add_topping(scoop_size, *toppings):
+    
+    print("Adding toppings to a ",scoop_size,"scoop ice cream:")
+    print('Toopings in your ice cream: ',toppings)
+
+def make_shake(choice):
+    print(f"Making a {choice} shake.")
